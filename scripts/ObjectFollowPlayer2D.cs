22e99043e5c3dcc9a68adecfcb9df7b58f8ce67c
kaiser0avm <kaiser.avm@gmail.com>
2014-11-02 11:13:37 -0600

    Object Follow Player Script
    Its a script used so the object follows the players while he moves, to
    use it you add it to the object you want that follows the player, then
    add the player game object and modify the properties in order to follow
    the player on x or y and with an offset

---
+++ scripts/ObjectFollowPlayer2D.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectFollowPlayer2D : MonoBehaviour {
+
+	public bool followOnX = false;
+	public bool followOnY = false;
+	public float offsetX = 0f;
+	public float offsetY = 0f;
+	public GameObject player;	
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null){
+			if(followOnX && followOnY){
+				this.transform.position = new Vector3 (player.transform.position.x + offsetX, player.transform.position.y + offsetY, this.transform.position.z);
+			}
+			else if(followOnX){
+				this.transform.position = new Vector3(player.transform.position.x + offsetX,this.transform.position.y,this.transform.position.z); 
+			}
+			else if(followOnY){
+				this.transform.position = new Vector3(this.transform.position.x,player.transform.position.y + offsetY, this.transform.position.z); 
+			}
+		}
+		else{
+			Debug.LogError("Player was not initialized");
+		}
+	}
+}
